e29661b11d06ab478d648b2a0ca534ffdab77e2c
IHRSI <147341843+IHRSI@users.noreply.github.com>
2024-12-16 18:33:52 +0530

    Done
    Pushback use

---
+++ 1678_Goal_Parser_Interpretation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    string interpret(string command) {
+        string s="";
+        for(int i=0;i<command.length();i++){
+            if(command[i]=='G') s+="G";//s.push_back('G');
+            else if(command[i+1]==')'){
+                s+="o";// s.push_back('o');
+                i+=1;
+            }
+            else{
+                s+="al";
+                i+=3;
+            }
+        }
+        return s;
+    }
+};
