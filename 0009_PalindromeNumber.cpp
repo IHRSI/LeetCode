147a2598f2ff8eb559122a493611ae614118c4a2
IHRSI <147341843+IHRSI@users.noreply.github.com>
2024-12-16 18:27:16 +0530

    Done

---
+++ 0009_PalindromeNumber.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0) return 0;
+        long r=0,t=x;
+        while(t>0){
+            r=r*10 + t%10;
+            t=t/10;
+        }
+        return r==x;
+    }
+};
