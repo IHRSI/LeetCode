baab162eb1ab9729d70ce68b2bed2dce5906400d
IHRSI <147341843+IHRSI@users.noreply.github.com>
2024-12-27 02:29:47 +0530

    Done-Hashing,UMap or Bit manipulation(XOR)

---
+++ 0136_Single_Number.cpp
@@ -0,0 +1,24 @@
+class Solution {// TC- O(n) SC- O(n) - Not much effcient this Q needed bit manipulation
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int &ele : nums){
+            m[ele]+=1;
+        }
+        for(auto &pr : m){
+            if(pr.second==1) return pr.first;
+        }
+        return 0;
+    }
+};
+// Better soln - x^x=0 and x^0=x using this concept
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int val : nums){
+            ans = ans ^ val;
+        }
+        return ans;
+    }
+};
