929bf1b78c4142bb51a2db51662b03dc97b26943
IHRSI <147341843+IHRSI@users.noreply.github.com>
2025-01-04 02:20:11 +0530

    Done - Used vector(brute force),UM(efiicient tc)

--- 0496_Next_Greater_Element.cpp
+++ 0496_Next_Greater_Element.cpp
@@ -1,5 +1,5 @@
-//Brute Force method
-class Solution { // TC- O(n1*n2*n2) , SC- O(n1)
+//Brute Force method, Used vector
+class Solution { // TC- O(n1*n2*n2) , SC- O(n1) , Better SC
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1=nums1.size();
@@ -17,6 +17,33 @@ public:
                     break;
                 }
             }
+        }
+        return ans;
+    }
+};
+
+//Efficient TC method , Used UM
+class Solution { // TC -O(n2) , SC-O(n1+n2)
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2= nums2.size();
+        stack<int> st;
+        unordered_map<int,int> nge;
+        for (int i = 0; i < n2; i++) {
+            while (!st.empty() && nums2[i] > nums2[st.top()]) {
+                nge[nums2[st.top()]] = nums2[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            nge[nums2[st.top()]]=-1;
+            st.pop();
+        }
+        vector<int> ans;
+        for(int ele: nums1){
+            ans.push_back(nge[ele]);
         }
         return ans;
     }
