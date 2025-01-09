294033212a06098361a1066dbf050fb50f564289
IHRSI <147341843+IHRSI@users.noreply.github.com>
2025-01-09 11:01:55 +0530

    Done- BinarySearch(used LowerBound)

---
+++ 0035_Search_Insert_Positon.cpp
@@ -0,0 +1,34 @@
+class Solution { //TC=O(logn) , SC=O(1)
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        auto ele=lower_bound(nums.begin(),nums.end(),target);
+        return ele - nums.begin();
+    }
+};
+
+//detailed soln withiut using lower_bound()
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size();
+        int mid;
+        if(target>nums[high-1]){
+            return high;
+        }
+        while(low<=high){
+              mid=(low+high)/2;
+            if(nums[mid]==target){  
+                return mid;
+            }
+          
+            if(target<nums[mid]){     
+            high=mid-1;    
+            }else{
+            low=mid+1;        
+            }
+          
+        }
+         return  low;   
+    }
+};
