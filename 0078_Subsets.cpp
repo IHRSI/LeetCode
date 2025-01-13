3bca8fa8f0a8b7cdb2f01f74001db64cd5585597
IHRSI <147341843+IHRSI@users.noreply.github.com>
2025-01-13 14:19:39 +0530

    HalfDone - Used l1-Backtracing,l2 - bitmasking(left)

---
+++ 0078_Subsets.cpp
@@ -0,0 +1,24 @@
+//Both methods are efficient
+//Used backtracking
+class Solution {// TC=SC=O(n*2^n)
+public:
+    vector<vector<int>> ans;
+    void subset(vector<int> &v,int i,vector<int> &nums){
+        if(i==nums.size()){
+            ans.push_back(v);
+            return;
+        }
+        subset(v,i+1,nums);
+        v.push_back(nums[i]);
+        subset(v,i+1,nums);
+        v.pop_back();
+        return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> v;
+        subset(v,0,nums);
+        return ans;
+    }
+};
+
+//Using Bitmasing
