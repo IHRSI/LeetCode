c16458b07cc7f2ea3971abbd9d2e636be2e77c27
IHRSI <147341843+IHRSI@users.noreply.github.com>
2025-01-24 00:33:04 +0530

    Done - Used L1-2pointer, dp?(not sure)

--- 0121.Best_Time_to_Buy_and_Sell_Stock.cpp
+++ 0121.Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,7 +1,7 @@
 //Using 2 pointer not efficient
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) { // TC=O(n), SC=(1)
+    int maxProfit(vector<int>& prices) { // TC=O(n) 19ms, SC=(1)
         if(prices.size()==1) return 0;
         int ans=0;
         int l=0,r=1;
@@ -16,5 +16,20 @@ public:
             }
         }
         return ans;
+    }
+};
+
+// Efficient
+class Solution { // TC=O(n) 0ms, SC=(1)
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = prices[0];
+        int maxprof = 0;
+
+        for(int i=1;i<prices.size();i++){
+            maxprof = max(maxprof,prices[i]-min_price);
+            min_price = min(prices[i],min_price);
+        }
+        return maxprof;
     }
 };
