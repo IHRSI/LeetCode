c4b01b36790be9bbd57b49d2a02237cf2ad0f7d4
IHRSI <147341843+IHRSI@users.noreply.github.com>
2025-01-25 01:16:35 +0530

    Done - Used Two pointer

---
+++ 0011_Container_With_Most_Water.cpp
@@ -0,0 +1,15 @@
+//Efficient code using two pointer
+class Solution { // TC-O(n), SC=O(1)
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size();
+        int l=0,r=n-1;
+        int ans=0;
+        while(l<r){
+            ans=max(ans,min(height[l],height[r])*(r-l));
+            if(height[l]>height[r]) r--;
+            else l++;
+        }
+        return ans;
+    }
+};
