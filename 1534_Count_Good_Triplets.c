d8dd984df0b7d448aa8aa60c83ac3695ad961bf5
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-05-24 19:49:53 +0530

    Done - Basic logic

---
+++ 1534_Count_Good_Triplets.c
@@ -0,0 +1,38 @@
+class Solution { //TC= Improved logic
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int ans = 0;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (abs(arr[i] - arr[j]) <= a){// see here
+                    for (int k = j + 1; k < n; k++) {
+                        if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
+                            ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution { // TC - O(n^3) , SC=O(1)
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int ans = 0;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (abs(arr[i] - arr[j]) <= a &&
+                        abs(arr[j] - arr[k]) <= b &&
+                        abs(arr[i] - arr[k]) <= c) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
