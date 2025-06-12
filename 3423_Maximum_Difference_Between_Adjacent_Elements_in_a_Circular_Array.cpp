74d5fdca64a7726265ade252809c26ee49bb6f96
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-06-12 17:07:32 +0530

    Done[POTD] - Used abs,max(easyQ)

---
+++ 3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array.cpp
@@ -0,0 +1,12 @@
+//VEasy
+class Solution {//TC=O(n) , SC=O(1)
+public:
+    int maxAdjacentDistance(vector<int>& nums) {
+        int n=nums.size();
+        int ans=abs(nums[n-1]-nums[0]);
+        for(int i=0;i<n-1;i++){
+            ans=max(ans,abs(nums[i]-nums[i+1]));
+        }
+        return ans;
+    }
+};
