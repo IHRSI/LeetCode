a3fb2c4b1be66bafb560f66a37dd62d53f75573b
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-06-14 20:17:23 +0530

    Done[POTD] - Used stoi,to_string(),greedy,math

---
+++ 2566_Maximum_Difference_by_Remapping_a_Digit.cpp
@@ -0,0 +1,23 @@
+class Solution {//TC=O(logn) : n=no of digits , SC=O(logn)
+public:
+    int minMaxDifference(int num) {
+        string max=to_string(num);//O(logn)
+        int n=max.length();
+        string min=max;
+        char max_ele='0';
+        char fst_ele=min[0];
+        for(int i=0;i<n;++i){//O(logn)
+            if(max[i]=='9') continue;
+            else{
+                max_ele=max[i];
+                break;
+            }
+        }
+        for(int i=0;i<n;++i){//O(logn)
+            if(fst_ele==min[i]) min[i]='0';
+            if(max_ele==max[i]) max[i]='9';
+        }
+
+        return stoi(max)-stoi(min);//O(logn)
+    }
+};
