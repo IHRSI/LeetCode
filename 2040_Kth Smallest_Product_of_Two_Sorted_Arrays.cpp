c2498fdf78ae1ce9c225fdea5fc337d98c640344
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-06-26 00:48:03 +0530

    Update 2040_Kth Smallest_Product_of_Two_Sorted_Arrays.cpp

--- 2040_Kth Smallest_Product_of_Two_Sorted_Arrays.cpp
+++ 2040_Kth Smallest_Product_of_Two_Sorted_Arrays.cpp
@@ -1,1 +1,16 @@
-.
+class Solution {// Used max heap gave TLE
+public:
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {//TC=O(n^2*logk) SC=O(K)
+        int n1=nums1.size();
+        int n2=nums2.size();
+        long long ans;
+        priority_queue<long long> mh;
+        for(int &i : nums1){
+            for(int &j: nums2){
+                mh.push(i*1ll*j);
+                if(mh.size()>k) mh.pop();
+            }            
+        }
+        return mh.top();
+        }
+};
