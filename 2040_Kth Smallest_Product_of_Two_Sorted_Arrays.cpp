a732e500989379c38090e027c8b7149cd38148f9
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-06-27 17:45:00 +0530

    Done(HARD)[POTD] - Used BS*BS , MaxHeap(TLE),math

--- 2040_Kth Smallest_Product_of_Two_Sorted_Arrays.cpp
+++ 2040_Kth Smallest_Product_of_Two_Sorted_Arrays.cpp
@@ -1,4 +1,7 @@
-//Binary Search inside Binary Search
+//Best approach was BS + Divide and coquer TC=O((n1+n2)logC)) i didnt understand this - This Q hasnt come in any company so not trying to understad this approach.
+
+//Binary Search inside Binary Search - half open BS(on ans - minimising the range )*Inclusive BS(on arr2 counting)
+
 /*If x 1≥0, then nums 2[j]×x1  forms a non-decreasing sequence. We use binary search to count how many products are ≤v.
 If x1<0, then nums 2[j]×x1 forms a non-increasing sequence. In this case, we use binary search to find how many products 
 are greater than v, denoted as t. Then, the number of products ≤v is n 2−t, where n 2 is the length of nums2.
@@ -7,14 +10,14 @@ class Solution {//TC=O(n1*logn2*logC) , SC=O(1)
 public:
     int f(vector<int>& nums2, long long x1, long long v) {
         int n2 = nums2.size();
-        int left = 0, right = n2;//[l,r)
-        while (left < right) {//half opened BS - counting
+        int left = 0, right = n2-1;
+        while (left <= right) {//Inclusive BS - counting
             int mid = (left + right) >> 1;
             if (x1 >= 0 && nums2[mid] * x1 <= v ||// less than equal to v
                 x1 < 0 && nums2[mid] * x1 > v) {// greater than v
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid -1;
             }
         }
         if (x1 >= 0) {
