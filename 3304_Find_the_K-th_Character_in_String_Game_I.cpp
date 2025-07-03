e8d5d070551048562460723742c4a67865591454
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-07-04 02:00:01 +0530

    Done[POTD] - Used O(1)~bitmani(pop_ct), simulation, global vari declare

--- 3304_Find_the_K-th_Character_in_String_Game_I.cpp
+++ 3304_Find_the_K-th_Character_in_String_Game_I.cpp
@@ -1,3 +1,16 @@
+//Great soln - good use of bit manipaulation thinking
+//doing k-1 to make it 0 indexed
+//The key observation is that:
+//Each operation effectively adds the count of 1s in the binary representation of (k - 1) to the starting character 'a'
+//The number of operations needed corresponds to the number of bits in (k - 1)'s binary representation
+class Solution {//TC=O(1) , SC=O(1)
+public:
+    char kthCharacter(int k) {//****The no of jumps the index does is always in pow of 2 so at all jumps any bit get sets thats the catch
+        int shift=__builtin_popcount(k-1);//O(1)
+        return 'a'+shift;
+    }
+};
+
 //My initial thought - Simulation
 class Solution {//TC=O(2K) - worst case =O(K) , SC=O(2K)=O(k);
 public:
@@ -8,12 +21,24 @@ public:
          while(ct<k){
             string temp="";
             for(auto s: str){
-                temp.push_back(s+1);
+                char ch= s=='z'? 'a': s+1;
+                temp.push_back(ch);
             }
             ct<<=1;
             str+=temp;
          }
          ans=str[k-1];
         return ans;
+    }
+};
+
+//Since the constraint is 1≤k≤500, we don't need to worry about performing modulus 26 operations explicitly.
+//First 500 letters 
+//This obvisouly i cant generate but pasted this soln to show the SC diffrence it does in defining globally.
+const string a="abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij";
+class Solution {//TC=O(1) , BUT SC is O(n=500)but beats 100% as its defined as const globally
+public:
+        char kthCharacter(int k) {
+        return a[k-1];
     }
 };
