f8427f2a2e88a40bfd959d30759f7cef9c5af710
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-07-06 20:06:19 +0530

    Done(MED)[POTD] - Used design(OOP syntax),2sum logic,hashing(UM)

--- 1865_Finding_Pairs_With_a_Certain_Sum.cpp
+++ 1865_Finding_Pairs_With_a_Certain_Sum.cpp
@@ -1,22 +1,24 @@
 //OOP concept
+/*Since the length of the array nums1 is less than or equal to that of nums2,for the getPairs(tot) operation, we can store the elements of nums2
+​in a hash map. Then, we enumerate each element num in nums1 and look up the value corresponding to the key tot−num in the hash map. The sum of these values gives us the answer.*/
 class FindSumPairs {//2sum logic
 private:
-    unordered_map<int,int> freq;
-    vector<int> nums1,nums2;
+    unordered_map<int,int> freq;//O(m)
+    vector<int> nums1,nums2;//SC-O(n+m)
 public:
-    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {//TC=O(nums1) , SC=O(nums2)
-        this->nums1=nums1;
-        this->nums2=nums2;
-        for(int &val: nums2) ++freq[val];
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {//TC=(n+2m+q1+q2*n) , SC=O(n+2m)
+        this->nums1=nums1;//O(n)
+        this->nums2=nums2;O//O(m)
+        for(int &val: nums2) ++freq[val];//O(m)
     }
-    
-    void add(int index, int val) {
+    //If we choose to delete keys from the hash map when their frequency becomes zero, the map’s space usage remains bounded by O(m), independent of the number of add operations.
+    void add(int index, int val) {//q1 calls - O(1)
         --freq[nums2[index]];
         nums2[index]+=val;
         ++freq[nums2[index]];
     }
     
-    int count(int tot) {
+    int count(int tot) {//q2 calls - O(n)
         int left,ans=0;
         for(int &val: nums1){
             left=tot-val;
