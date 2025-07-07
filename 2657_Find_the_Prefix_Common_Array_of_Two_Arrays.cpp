e971969af4b0e604726f0d463f60cf0227a0ccc2
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-07-08 00:38:34 +0530

    Done(MED) - Used hashing(vector<bool>) , bitmasking(n<64)

--- 2657_Find_the_Prefix_Common_Array_of_Two_Arrays.cpp
+++ 2657_Find_the_Prefix_Common_Array_of_Two_Arrays.cpp
@@ -15,8 +15,25 @@ public:
             ans.push_back(common);
         }
         return ans;
-
     }
 };
 
-//BitMasking - Better SC=O(1)
+//BitMasking - Better SC=O(1) ,TC=O(n) but improved the runtime due to fast operations.
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n=A.size();
+        vector<int> ans;
+        long long bitset=0;
+        int common=0;
+        for(int i=0; i<n;++i){
+            if(bitset & (1LL<<A[i])) common++;
+            bitset|=(1LL<<A[i]);
+            if(bitset & (1LL<<B[i])) common++;
+            bitset|=(1LL<<B[i]);
+            ans.push_back(common);
+        }
+        return ans;
+
+    }
+};
