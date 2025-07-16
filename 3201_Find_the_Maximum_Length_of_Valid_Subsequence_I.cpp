10a7e56d8de3518067f554f7cf8726386b840842
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-07-16 20:25:56 +0530

    Done(MED)[POTD] - Used greedy parity enumeration

--- 3201_Find_the_Maximum_Length_of_Valid_Subsequence_I.cpp
+++ 3201_Find_the_Maximum_Length_of_Valid_Subsequence_I.cpp
@@ -1,3 +1,13 @@
+//Q mentioned DP but used greedy parity enumeration
+/*
+All you need to do is:
+While traversing the array, count the number of even numbers and the number of instances where the current number
+is the opposite parity of the one before it (i.e. if nums[i - 1] is even and nums[i] is odd, or vice versa)
+Return whichever value is largest: the number of even numbers, the number of odd numbers (calculated by subtracting
+the number of even numbers from the length of the nums), or the number of opposite parities.
+*/
+//Give this a thought then get the intution. As mod 2 gives only values 0 or 1.
+//(sub[0] + sub[1]) % 2 == (sub[1] + sub[2]) % 2 == ... == (sub[x - 2] + sub[x - 1]) % 2.
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {//TC=O(n) , SC=O(1)
