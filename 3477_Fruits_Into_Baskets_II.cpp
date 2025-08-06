1b19fe3f7c14a2f6fa7aa96946f229d6309b9a72
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-08-06 09:33:18 +0530

    Done[POTD] - Used simulation,ordered set

--- 3477_Fruits_Into_Baskets_II.cpp
+++ 3477_Fruits_Into_Baskets_II.cpp
@@ -1,1 +1,46 @@
+//Initial thought
+class Solution {//TC=O(n^2), SC=O(1)
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n=fruits.size();
+        int left=n;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<n;++j){
+                if(baskets[j]==0) continue;
+                if(fruits[i]<=baskets[j]){
+                    --left;
+                    baskets[j]=0;
+                    break;
+                }
+            }
+        }
+        return left;
+    }
+};
 
+//Used ordered set
+class Solution {//TC=O(n^2+nlogn) , SC=O(n)
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        set<int> unusedIndices;
+        for (int i = 0; i < n; ++i)
+            unusedIndices.insert(i); // all baskets initially unused
+
+        int unplaced = 0;
+
+        for (int i = 0; i < n; ++i) {
+            bool placed = false;
+            for (int j : unusedIndices) {
+                if (baskets[j] >= fruits[i]) {
+                    unusedIndices.erase(j); // use the basket
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) ++unplaced; // fruit couldn't be placed
+        }
+
+        return unplaced;
+    }
+};
