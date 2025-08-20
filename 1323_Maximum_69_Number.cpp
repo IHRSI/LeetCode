6b0fc85b94118daf7813948c513518e101c217b9
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-08-20 23:19:55 +0530

    Done[POTD] - Used stoi,to_str,greedy

--- 1323_Maximum_69_Number.cpp
+++ 1323_Maximum_69_Number.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int maximum69Number (int num) {//TC=O(log(num)) , SC=O(log(num))
+        string s=to_string(num);
+        for(char &c: s){
+            if(c=='6'){
+                c='9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
