24d75e174834555b3e8bc5e5bb87588ab16ea86f
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-08-25 11:27:52 +0530

    Done(MED)[POTD] - Used slid window,array

--- 1493_Longest_Subarray_of_1's_After_Deleting_One_Element.cpp
+++ 1493_Longest_Subarray_of_1's_After_Deleting_One_Element.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {//TC=O(2n)=O(n) , SC=O(1)
+        int ans=0;
+        int ct0=0;
+        int srt=0;
+        int n=nums.size();
+        for(int i=0;i<n;++i){
+            if(nums[i]==0) ++ct0;
+            while(ct0>1){
+                if(nums[srt]==0) --ct0;
+                ++srt;
+            }
+            ans=max(ans,i-srt);
+        }
+        return ans;
+    }
+};
