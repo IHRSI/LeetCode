d88a77e616b2e979b22c3f5a64932bec877d8c95
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-08-29 21:25:10 +0530

    Done[POTD] - Used math,array

--- 3000_Maximum_Area_of_Longest_Diagonal_Rectangle.cpp
+++ 3000_Maximum_Area_of_Longest_Diagonal_Rectangle.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-        int maxDiaSq = 0, maxArea = 0;
-        for (const auto& dim : dimensions) {
-            int l = dim[0], w = dim[1];
-            int diaSq = l * l + w * w, area = l * w;
-            if (diaSq > maxDiaSq) {
-                maxDiaSq = diaSq;
-                maxArea = area;
-            } else if (diaSq == maxDiaSq) {
-                maxArea = max(maxArea, area);
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {//TC=O(n), SC=O(1)
+        int n=dimensions.size();
+        int max_dig_sq=0;
+        int ans=0;
+        int a,b,temp_sq;
+        for(int i=0;i<n;++i){
+            a=dimensions[i][0];
+            b=dimensions[i][1];
+            temp_sq=a*a+b*b;
+            if(temp_sq>max_dig_sq){
+                max_dig_sq=temp_sq;
+                ans=a*b;
+            }
+            else if(temp_sq==max_dig_sq){
+                ans=max(ans,a*b);
             }
         }
-        return maxArea;
+        return ans;
     }
 };
+/*
+You compute the diagonal, convert it to an int, and compare.
+But sqrt() returns a double.
+When you cast it to int, you lose precision, and two rectangles with slightly different diagonals could end up considered equal incorrectly.
+eg - [[10,3],[5,9],[8,3]] therefore we compared squared diagonals directly.
+*/
