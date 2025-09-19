bf0b1391e6e2e0337d2de0a35967b10204df3417
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-09-19 23:59:43 +0530

    Done(MED)[POTD] - Used bitmask,any_of(lambda),math,string

--- 3227_Vowels_Game_in_a_String.cpp
+++ 3227_Vowels_Game_in_a_String.cpp
@@ -3,6 +3,7 @@ public:
     bool doesAliceWin(string s) {//TC=O(n), SC=O(1)
         return any_of(s.begin(),s.end(),[](char c){
             return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+            // return (0x104111 >> (c - 97)) & 1;//Better soln (faster), Used bitmask of letters of alphabet
         });
     }
 };
