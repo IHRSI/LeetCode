4ce0c5bd719a727f97cc2c471782843663ea9460
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-09-25 19:30:09 +0530

    Add solution for counting typeable words
    Implement solution to count typeable words considering broken letters.

--- 1935_Maximum_Number_of_Words_You_Can_Type.cpp
+++ 1935_Maximum_Number_of_Words_You_Can_Type.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {//TC=O(n*m), SC=O(n)
+        int ans=0;
+        string s;
+        stringstream ss(text);
+        while(ss>>s){
+        //while(getline(ss,s,' ')){
+            bool present=false;
+            for(char c: brokenLetters){
+                if(s.find(c)!=string ::npos){
+                    present=true;
+                    break;
+                }
+            }
+            if(!present) ++ans;
+        }
+        return ans;
+    }
+};
