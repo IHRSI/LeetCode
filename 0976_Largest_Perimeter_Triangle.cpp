ad971b34b59d68c8862c6a66bacdb4d1be0be1fc
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-10-02 22:07:20 +0530

    Done[POTD] - Used greedy,sort,math,array

--- 0976_Largest_Perimeter_Triangle.cpp
+++ 0976_Largest_Perimeter_Triangle.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int largestPerimeter(vector<int>& nums) {//TC=O(nlogn)
+    int largestPerimeter(vector<int>& nums) {//TC=O(nlogn) ,  SC=O(logn) - due to recursion depth 
         int n=nums.size();
         sort(nums.begin(),nums.end());
         for(int i=n-3;i>=0;--i){
