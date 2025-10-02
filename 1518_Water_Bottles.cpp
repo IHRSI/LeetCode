d313bca3e764e1447960d60a6a18d9225715f79b
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-10-02 21:59:14 +0530

    Done[POTD] - Used math(logic),simulation

--- 1518_Water_Bottles.cpp
+++ 1518_Water_Bottles.cpp
@@ -1,1 +1,33 @@
+//By me - efficient
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {//TC=O(n), SC=O(1)
+        int ans=numBottles;
+        while(numBottles>=numExchange){
+            int quo=numBottles/numExchange;
+            ans+=quo;
+            numBottles=numBottles%numExchange+quo;
+        }
+        return ans;
+    }
+};
 
+//Simulation
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {//TC=O(n), SC=O(1) - Refer editorial
+        int consumedBottles = 0;
+
+        while (numBottles >= numExchange) {
+            // Consume numExchange full bottles.
+            consumedBottles += numExchange;
+            numBottles -= numExchange;
+
+            // Exchange them for one full bottle.
+            numBottles++;
+        }
+
+        // Consume the remaining numBottles (<numExchange).
+        return consumedBottles + numBottles;
+    }
+};
