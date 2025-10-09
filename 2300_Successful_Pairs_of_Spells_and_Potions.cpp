a9967267f4b87412dc4e79580dcfa40b6b2c372c
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-10-09 21:03:04 +0530

    Done(MED)[POTD] - Predicate fn(BS), 2ptr,greddy,sorting,array
    Implemented a solution to calculate successful pairs of spells and potions based on a success threshold.

--- 2300_Successful_Pairs_of_Spells_and_Potions.cpp
+++ 2300_Successful_Pairs_of_Spells_and_Potions.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {//TC=O(nlogm+mlogm) , SC=O(1)
+        int n=spells.size();
+        int m=potions.size();
+        vector<int> ans(n);
+        sort(potions.begin(),potions.end());
+        for(int i=0;i<n;++i){
+            int val=spells[i];
+            int l=0,r=m-1;
+            while(l<=r){
+                int mid=l+((r-l)>>1);
+                if(val*1.0*potions[mid]>=success) r=mid-1;
+                else l=mid+1;
+            }
+            ans[i]=m-l;
+        }
+        return ans;
+    }
+};
