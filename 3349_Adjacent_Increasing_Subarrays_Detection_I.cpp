d0f5941d13b55512df9c2bf3761703586155fb6b
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-10-16 23:31:42 +0530

    Done[POTD] - array,good logic

--- 3349_Adjacent_Increasing_Subarrays_Detection_I.cpp
+++ 3349_Adjacent_Increasing_Subarrays_Detection_I.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {//TC=O(n), SC=O(1)
+        int n=nums.size();
+        int ct=1;
+        int prevct=0;
+        for(int i=1;i<n;++i){
+            if(nums[i]>nums[i-1]){
+                ++ct;
+            }
+            else{
+                prevct=ct;
+                ct=1;
+            }
+            if(ct>=2*k) return true;
+            if(min(ct,prevct)>=k) return true;
+        }
+        return false;
+    }
+};
