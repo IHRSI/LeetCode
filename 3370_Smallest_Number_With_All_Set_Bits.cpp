3681bc6436f0d898f2c1d275896a19077f372c9f
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-10-31 19:51:50 +0530

    Done[POTD] - math,bit mani

--- 3370_Smallest_Number_With_All_Set_Bits.cpp
+++ 3370_Smallest_Number_With_All_Set_Bits.cpp
@@ -1,3 +1,4 @@
+//Bit mani my logic
 class Solution {
 public:
     int smallestNumber(int n) {//TC=O(logn) , SC=O(1)
@@ -8,5 +9,25 @@ public:
             n>>=1;
         }
         return (1<<ct)-1;
+    }
+};
+//AS only 11 possibility are there we can use lower bound also after forming the array of all possible ans.
+//Bit mani
+class Solution {
+public:
+    int smallestNumber(unsigned n) {//TC=O(1), SC=O(1)
+        return bit_ceil(n+1)-1;//O(1)
+    }
+};
+
+//Mathn pattern logic
+class Solution {
+public:
+    int smallestNumber(int n) {//TC=O(lohn), SC=O(1)
+        int x = 1;
+        while (x < n) {
+            x = x * 2 + 1;
+        }
+        return x;
     }
 };
