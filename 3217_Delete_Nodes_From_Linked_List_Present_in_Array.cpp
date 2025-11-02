48c89745f7488a86afb89c7e23b45361184db049
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-11-03 00:31:07 +0530

    Done(MED)[POTD] - LinkedList,hash(UnSet)

--- 3217_Delete_Nodes_From_Linked_List_Present_in_Array.cpp
+++ 3217_Delete_Nodes_From_Linked_List_Present_in_Array.cpp
@@ -1,3 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {//TC=O(n+m), SC=O(n)
@@ -12,5 +23,23 @@ public:
             curr=curr->next;
         }
         return head;
+    }
+};
+
+//Using dummy node that points to head to handle the separate case i.e if value belongs to the head only
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {//TC=O(n+m), SC=O(n)
+        unordered_set<int> us;
+        for(int e: nums) us.insert(e);
+        ListNode dummy(0,head);//addition
+        ListNode *prev=&dummy;
+        ListNode *curr=head;
+        while(curr!=nullptr){
+            if(us.find(curr->val)!=us.end()) prev->next=curr->next;
+            else prev=curr;
+            curr=curr->next;
+        }
+        return dummy.next;// change
     }
 };
