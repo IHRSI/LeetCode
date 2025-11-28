54236842841df3d7a2bb8a9d1b351abbfc49cca5
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-11-28 23:56:18 +0530

    Done[POTD] - hashing(vec),string,sorting

--- 2273_Find_Resultant_Array_After_Removing_Anagrams.cpp
+++ 2273_Find_Resultant_Array_After_Removing_Anagrams.cpp
@@ -28,3 +28,19 @@ public:
         return ans;
     }
 };
+
+//Using Sorting
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {//TC=O(n*mlogm), SC=(m), m is length of string
+        vector<string> ans;
+        ans.push_back(words[0]);
+        
+        for (int i = 1; i < words.size(); i++) {
+            string a = words[i], b = ans.back();
+            sort(a.begin(), a.end()), sort(b.begin(), b.end());
+            if (a != b) ans.push_back(words[i]); 
+        }
+        return ans;
+    }
+};
