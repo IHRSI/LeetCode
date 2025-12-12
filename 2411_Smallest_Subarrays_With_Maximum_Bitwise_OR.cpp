e3a467010dc5f99f07f1ea6ddc071058656779e1
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-12-12 14:52:29 +0530

    Done(MED)[POTD]- bit mani.subarr,right to left trav,

--- 2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.cpp
+++ 2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {//TC=O(n*32)~O(n), SC=O(31)~O(1)
+        int n=nums.size();
+        vector<int> ans(n);
+        vector<int> pos(31,-1);
+        for(int i=n-1;i>=0;--i){
+            int mxidx=i;
+            for(int bit=0;bit<31;++bit){
+                if(!(nums[i]& (1<<bit))){
+                    mxidx=max(mxidx,pos[bit]);
+                }
+                else pos[bit]=i;
+            }
+            ans[i]=mxidx-i+1;
+        }
+        return ans;
+    }
+};
