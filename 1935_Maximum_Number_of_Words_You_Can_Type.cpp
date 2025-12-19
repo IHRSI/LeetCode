03d6cd0e17cad298d32aae3de5491839abf8dd46
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-12-19 17:08:48 +0530

    Done[POTD]- string(ss),hash(vector<bool>)

--- 1935_Maximum_Number_of_Words_You_Can_Type.cpp
+++ 1935_Maximum_Number_of_Words_You_Can_Type.cpp
@@ -1,6 +1,51 @@
 class Solution {
 public:
-    int canBeTypedWords(string text, string brokenLetters) {//TC=O(n*m), SC=O(n)
+    int canBeTypedWords(string text, string brokenLetters) {//TC=O(m+n), SC=O(26)=O(1)
+        vector<bool> broke(26,false);
+        for(char c: brokenLetters){
+            broke[c-'a']=true;
+        }
+        int ans=0;
+        bool flag=true;
+        for(char c: text){
+            if(c==' '){
+                if(flag) ++ans;
+                flag=true;
+            }
+            else if(broke[c-'a']) flag=false;
+        }
+        if(flag) ++ans;
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {//TC=O(n+m), SC=O(n+26)
+        vector<bool> broke(26,false);
+        for(char c: brokenLetters){//O(m)
+            broke[c-'a']=true;
+        }
+        stringstream ss(text);//Internally scans the entire string once O(n)
+        string token;
+        int ans=0;
+        while(ss>> token){//O(n)
+            int isvalid=true;
+            for(char c: token){
+                if(broke[c-'a']){
+                    isvalid=false;
+                    break;
+                }
+            }
+            if(isvalid) ++ans;
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {//TC=O(n*26), SC=O(n)
         int ans=0;
         string s;
         stringstream ss(text);
@@ -14,6 +59,32 @@ public:
                 }
             }
             if(!present) ++ans;
+        }
+        return ans;
+    }
+};
+//Without using stringstream
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {//TC=O(n*26), SC=O(n)
+        int ans=0;
+        int n=text.size();
+        int i=0;
+        while(i<n){
+            string s;
+            while(i<n && text[i]!=' '){
+                s+=text[i];
+                ++i;
+            }
+            bool present=false;
+            for(char c: brokenLetters){
+                if(s.find(c)!=string ::npos){
+                    present=true;
+                    break;
+                }
+            }
+            if(!present) ++ans;
+            ++i;
         }
         return ans;
     }
