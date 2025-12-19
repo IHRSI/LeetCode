43c959bf2e55dc0f5be0666e16c0df06904d8b39
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-12-20 01:43:30 +0530

    Done(MED)[POTD]- counting sort,string(ASCCI),2ptr

--- 2785_Sort_Vowels_in_a_String.cpp
+++ 2785_Sort_Vowels_in_a_String.cpp
@@ -4,6 +4,31 @@ public:
         return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
         c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
     }
+    string sortVowels(string s) {//TC=O(n), SC=O(1)
+        int n=s.size();
+        unordered_map<char,int> ct;//O(10)
+        for(char c: s){
+            if(isvov(c)) ct[c]++;
+        }
+        string sortedvov="AEIOUaeiou";
+        int j=0;
+        for(int i=0;i<n;++i){
+            if(isvov(s[i])){
+                while(ct[sortedvov[j]]==0) ++j;
+                s[i]=sortedvov[j];
+                ct[sortedvov[j]]--;
+            }
+        }
+        return s;
+    }
+};
+
+class Solution {
+public:
+    bool isvov(char c){
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
+        c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
+    }
     string sortVowels(string s) {//TC=O(nlogn), SC=O(n)
         int n=s.size();
         string vovstr;//SC=O(n) in worst case
