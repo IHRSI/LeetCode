cf0535e5007236472f6a5cb2adee7aebfa6678ec
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2025-12-25 02:58:20 +0530

    Done[POTD]- greedy,counting sort,sort

--- 3074_Apple_Redistribution_into_Boxes.cpp
+++ 3074_Apple_Redistribution_into_Boxes.cpp
@@ -1,3 +1,25 @@
+//Counting Sort
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {//TC=O(n+m), SC=O(1)
+        int sum=accumulate(apple.begin(),apple.end(),0);
+        int boxct=0;
+        vector<int> ct(51);
+        for(int a:capacity) ++ct[a];
+        for(int i=50;i>=1;--i){
+            if(ct[i]!=0){
+                while(ct[i]>0){
+                    sum-=i;
+                    ++boxct;
+                    if(sum<=0) return boxct;
+                    ct[i]--;
+                }
+            }
+        }
+        return 0;
+    }
+};
+
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {//TC=O(n+mlogm), SC=O(logm)
