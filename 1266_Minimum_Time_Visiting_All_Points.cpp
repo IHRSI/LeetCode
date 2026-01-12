966feb507c1fef0d1154e021a872f990e9cb0be9
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-01-13 00:32:28 +0530

    Done- geometry,math

---
+++ 1266_Minimum_Time_Visiting_All_Points.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {//TC=O(n), SC=O(1)
+        int time=0;
+        int n=points.size();
+        for(int i=0;i<n-1;++i){
+            int x1=points[i][0];
+            int y1=points[i][1];
+            int x2=points[i+1][0];
+            int y2=points[i+1][1];
+            time+=max(abs(x1-x2),abs(y1-y2));
+        }
+        return time;
+    }
+};
