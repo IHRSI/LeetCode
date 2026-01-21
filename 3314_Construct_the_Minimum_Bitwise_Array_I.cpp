f9dc9e7d9bc1ada7ae745ede02d48518c8338067
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-01-21 11:13:03 +0530

    Done[POTD]- bit mani

--- 3314_Construct_the_Minimum_Bitwise_Array_I.cpp
+++ 3314_Construct_the_Minimum_Bitwise_Array_I.cpp
@@ -1,6 +1,6 @@
-class Solution {
+class Solution {//Brute Force
 public:
-    vector<int> minBitwiseArray(vector<int>& nums) {//TC=O(n*m), SC=O(1), m is largest  no. in array
+    vector<int> minBitwiseArray(vector<int>& nums) {//TC=O(n*max(nums[i])), SC=O(1)
         for(int i=0;i<nums.size();++i){
             int temp=nums[i];
             for(int j=1;j<temp;++j){
