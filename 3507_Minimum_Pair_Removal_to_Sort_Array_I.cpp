63945aa95aa3ef5383c114e171f0d598c20f4d15
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-01-28 10:58:39 +0530

    Done[POTD]- simulation, LL simul in array

--- 3507_Minimum_Pair_Removal_to_Sort_Array_I.cpp
+++ 3507_Minimum_Pair_Removal_to_Sort_Array_I.cpp
@@ -19,3 +19,39 @@ public:
         return ans;
     }
 };
+
+//Making the erase as O(1) using SC by using linked list logic/implementation in array
+class Solution {
+public:
+    int minimumPairRemoval(vector<int>& nums) {//TC=O(n^2), SC=O(n)
+        int n = nums.size();
+        vector<int> next(n, -1);
+        // build linked list
+        for (int i = 0; i < n - 1; ++i)
+            next[i] = i + 1;
+        int ans = 0;
+        while (true) {
+            bool sorted = true;
+            int min_sum = INT_MAX;
+            int min_idx = -1;
+            // traverse list
+            for (int i = 0; next[i] != -1; i = next[i]) {
+                int j = next[i];
+                if (nums[i] > nums[j])
+                    sorted = false;
+                int s = nums[i] + nums[j];
+                if (s < min_sum) {
+                    min_sum = s;
+                    min_idx = i;
+                }
+            }
+            if (sorted) break;
+            // merge min_idx and next[min_idx]
+            int j = next[min_idx];
+            nums[min_idx] = min_sum;
+            next[min_idx] = next[j];   // O(1) deletion
+            ++ans;
+        }
+        return ans;
+    }
+};
