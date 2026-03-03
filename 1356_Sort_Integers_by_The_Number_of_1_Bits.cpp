ff3ecc7fc4dd92522fc1585088f124e71f95b682
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-03-04 00:47:44 +0530

    Done[POTD] - lambda(sort), bit mani, counting

--- 1356_Sort_Integers_by_The_Number_of_1_Bits.cpp
+++ 1356_Sort_Integers_by_The_Number_of_1_Bits.cpp
@@ -1,3 +1,25 @@
+class Solution {
+public:
+    //Brian Kerninghan's Algorithm
+    int hamming(int num){//Runs the number of times set bit is present in the num
+        int wt=0;//No. of 1 bits
+        while(num>0){
+            num&=(num-1);
+            ++wt;
+        }
+        return wt;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(),arr.end(),[&](int a, int b){
+            int x=hamming(a);
+            int y=hamming(b);
+            if(x==y) return a<b;
+            return x<y;
+        });
+        return arr;
+    }
+};
+
 //TC=O(nlogn), SC=O(logn)
 class Solution {
 public:
@@ -8,6 +30,12 @@ public:
             if(x>y) return false;
             else if(x==y) return a<b;
             return true;
+            /*
+            if (__builtin_popcount(a) == __builtin_popcount(b)) {
+            return a < b;
+            }
+            return __builtin_popcount(a) < __builtin_popcount(b);
+            */
         });
         return arr;
     }
