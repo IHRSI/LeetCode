9544154e36aa3e27d5a2b78d006c0e7e98a8ef7a
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-04-03 11:26:29 +0530

    Done(MED)[POTD] - prefix sum, stack, dp, string

--- 1653_Minimum_Deletions_to_Make_String_Balanced.cpp
+++ 1653_Minimum_Deletions_to_Make_String_Balanced.cpp
@@ -1,3 +1,59 @@
+//Optimised DP
+class Solution {
+public:
+    int minimumDeletions(string s) {//TC=O(n), SC=O(1)
+        int n = s.length();
+        int min_deletions = 0;
+        int b_count = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'b') {
+                b_count++;
+            } else {
+                min_deletions = min(min_deletions + 1, b_count);
+            }
+        }
+        return min_deletions;
+    }
+};
+
+//Stack - Single Pass
+class Solution {//TC=O(n), SC=O(n) worst case
+public:
+    int minimumDeletions(string s) {
+        int n=s.size();
+        stack<char> lifo;
+        int mindel=0;
+        for(int i=0;i<n;++i){
+            if(!lifo.empty() && lifo.top()=='b' && s[i]=='a'){
+                lifo.pop();
+                ++mindel;
+            }
+            else lifo.push(s[i]);
+        }
+        return mindel;
+    }
+};
+
+//2 pass
+class Solution {
+public:
+    int minimumDeletions(string s) {//TC=O(2n~n), SC=O(1)
+        int n = s.length();
+        int a_count = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a') a_count++;
+        }
+        int b_count = 0;
+        int min_deletions = a_count;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a') a_count--;
+            min_deletions = min(min_deletions, a_count + b_count);
+            if (s[i] == 'b') b_count++;
+        }
+        return min_deletions;
+    }
+};
+
 //Prefix Sum
 class Solution {
 public:
