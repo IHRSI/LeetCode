c7e694920d73218ca82754721fe598f47d1fca84
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-05-11 23:00:47 +0530

    Done[POTD] - simulation

--- 2553_Separate_the_Digits_in_an_Array.cpp
+++ 2553_Separate_the_Digits_in_an_Array.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
-    vector<int> separateDigits(vector<int>& nums) {
+    vector<int> separateDigits(vector<int>& nums) {//TC=O(n), SC=O(1)
+        int n=nums.size();
+        vector<int> ans;
+        for(int i=n-1;i>=0;--i){
+            int x=nums[i];
+            while(x>0){
+                ans.push_back(x%10);
+                x/=10;
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {//TC=O(nlogM), SC=O(logM) , where M is max value in arr
         int n=nums.size();
         vector<int> ans;
         for(int i=0;i<n;++i){
