fd4608c9a3bf7899fc52e7710ae361cb8a39a153
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-05-30 03:33:30 +0530

    Done(VEASY)[POTD] - math

--- 3300_Minimum_Element_After_Replacement_With_Digit_Sum.cpp
+++ 3300_Minimum_Element_After_Replacement_With_Digit_Sum.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
-    int minElement(vector<int>& nums) {//TC=O(nlogn), SC=O(1)
+    int minElement(vector<int>& nums) {//TC=O(nlogk), SC=O(1)
+        int n=nums.size();
+        int ans=37;
+        for(int i=0;i<n;++i){
+            int sum=0,e=nums[i];
+            while(e>0){
+                sum+=e%10;
+                e/=10;
+            }
+            ans=min(sum,ans);
+        }
+        return ans;
+    }
+};
+    
+class Solution {
+public:
+    int minElement(vector<int>& nums) {//TC=O(nlogn+n)~O(nlogk), SC=O(1)
         int n=nums.size();
         for(int i=0;i<n;++i){
             int sum=0,e=nums[i];
