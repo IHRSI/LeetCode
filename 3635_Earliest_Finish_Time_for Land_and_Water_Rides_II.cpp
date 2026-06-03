0b0e4ae2fdde7ddfe9449655b4ae62c092525056
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-06-03 12:48:17 +0530

    Done(MED)[POTD] - greedy

---
+++ 3635_Earliest_Finish_Time_for Land_and_Water_Rides_II.cpp
@@ -0,0 +1,21 @@
+class Solution {//TC=O(n+m), SC=O(1)
+public:
+    int solve(vector<int>& s1, vector<int>& d1, vector<int>& s2, vector<int>& d2){
+        int finish1=INT_MAX;
+        int n=s1.size();
+        for(int i=0;i<n;++i){
+            finish1=min(finish1,s1[i]+d1[i]);
+        }
+        int finish2=INT_MAX;
+        int m=s2.size();
+        for(int j=0;j<m;++j){
+            finish2=min(finish2,max(finish1,s2[j])+d2[j]);
+        }
+        return finish2;
+    }
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int landwater=solve(landStartTime,landDuration,waterStartTime,waterDuration);
+        int waterland=solve(waterStartTime,waterDuration,landStartTime,landDuration);
+        return min(landwater,waterland);
+    }
+};
