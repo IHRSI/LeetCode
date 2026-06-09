e4a6d0271945c7a27f154de626d27690d1130840
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-06-10 01:15:01 +0530

    Done{IMP}(MED)[POTD] - bin tree, hash(um<int,vec<int,bool>>), dfs, bfs

--- 2196_Create_Binary_Tree_From_Descriptions.cpp
+++ 2196_Create_Binary_Tree_From_Descriptions.cpp
@@ -9,9 +9,10 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Best Approach - Constructing Tree From Directly Map and TreeNode Object
 class Solution {
 public:
-    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {//TC=O(n), SC=O(n)
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {//TC=O(2n~O(n)), SC=O(2n)~O(n)
         int n=descriptions.size();
         unordered_map<int,TreeNode*> node;
         unordered_set<int> child;
@@ -28,5 +29,75 @@ public:
             if(child.count(e[0])==0) return node[e[0]];
         }
         return nullptr;
+    }
+};
+
+//BSF - TC=O(3n)~O(n), SC+O(4n)~O(n)
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_set<int> chi;
+        unordered_map<int,vector<pair<int,bool>>> parchi;
+        for(auto &e: descriptions){
+            int c=e[1];
+            chi.insert(c);
+            parchi[e[0]].emplace_back(c,e[2]);
+        }
+        TreeNode *root;
+        for(auto &e: descriptions){
+            if(chi.count(e[0])==0){
+                root=new TreeNode(e[0]);
+                break;
+            }
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* par=q.front();
+            q.pop();
+            for(auto &chi_info: parchi[par->val]){
+                int val=chi_info.first;
+                bool lr=chi_info.second;
+                TreeNode *newchi_n=new TreeNode(val);
+                q.push(newchi_n);
+                if(lr) par->left=newchi_n;
+                else par->right=newchi_n;
+            }
+        }
+        return root;
+    }
+};
+//DFS - TC=O(3n)~O(n), SC=O(4n)~O(n)
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_set<int> chi;
+        unordered_map<int,vector<pair<int,bool>>> parchi;
+        for(auto &e: descriptions){
+            int c=e[1];
+            chi.insert(c);
+            parchi[e[0]].emplace_back(c,e[2]);
+        }
+        int root;
+        for(auto &e: descriptions){
+            if(chi.count(e[0])==0){
+                root=e[0];
+                break;
+            }
+        }
+        return dfs(root,parchi);
+    }
+private:
+    TreeNode* dfs(int val,unordered_map<int,vector<pair<int,bool>>> &parchi){
+        TreeNode* par=new TreeNode(val);
+        if(parchi.find(val)!=parchi.end()){
+            for(auto &chinfo: parchi[val]){
+                int chival=chinfo.first;
+                bool lr=chinfo.second;
+                if(lr) par->left=dfs(chival,parchi);
+                else par->right=dfs(chival,parchi);
+            }
+        }
+        return par;
     }
 };
