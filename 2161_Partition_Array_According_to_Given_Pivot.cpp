643d3751839dd9c9661a23c3348fb1a9c3a1471b
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-06-10 15:22:39 +0530

    Done(MED)[POTD] - 2ptr, indexing

--- 2161_Partition_Array_According_to_Given_Pivot.cpp
+++ 2161_Partition_Array_According_to_Given_Pivot.cpp
@@ -1,3 +1,59 @@
+class Solution {//Efficient
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {//TC=O(2n)~O(n), SC=O(1)
+        int n=nums.size();
+        int less=0,eql=0;
+        for(int e: nums){
+            if(e<pivot) ++less;
+            else if(e==pivot) ++eql;
+        }
+        vector<int> ans(n);
+        int idx_l=0,idx_e=less,idx_m=less+eql;
+        for(int e: nums){
+            if(e<pivot){
+                ans[idx_l]=e;
+                ++idx_l;
+            }
+            else if(e==pivot){
+                ans[idx_e]=e;
+                ++idx_e;
+            }
+            else{
+                ans[idx_m]=e;
+                ++idx_m;
+            }
+        }
+        return ans;
+    }
+};
+
+//2ptr
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {//TC=O(3n)~O(n), SC=O(1)
+        int n=nums.size();
+        vector<int> ans(n);
+        int less=0,more=n-1;
+        for(int e: nums){
+            if(e<pivot){
+                ans[less]=e;
+                ++less;
+            }
+        }
+        for(int i=n-1;i>=0;--i){
+            if(nums[i]>pivot){
+                ans[more]=nums[i];
+                --more;
+            }
+        }
+        while(less<=more){
+            ans[less]=pivot;
+            ++less;
+        }
+        return ans;
+    }
+};
+
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {//TC=O(2n)~O(n), SC=O(n)
