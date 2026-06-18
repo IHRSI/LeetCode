b4feaa67993c1709d5a1aee2365d9c48cf551076
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-06-19 01:42:24 +0530

    Done(MED) - pq(min,max), tuple

--- 0373_Find_K_Pairs_with_Smallest_Sums.cpp
+++ 0373_Find_K_Pairs_with_Smallest_Sums.cpp
@@ -1,3 +1,27 @@
+//Efficient - Uses logic of Q23 - This implementation looks similar to that of Q23 min heap soln
+class Solution {
+public:
+    typedef tuple<int,int,int> t;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {//TC=O(min(n,k)*log(min(n,k))) + O(k*log(min(n,k))~O(klog(min(n,k))), SC=O(min(n,k))
+        int n=nums1.size(),m=nums2.size();
+        vector<vector<int>> ans;
+        priority_queue<t,vector<t>,greater<t>> pq;
+        for (int i = 0; i < nums1.size() && i < k; i++) {
+            pq.push({nums1[i] + nums2[0], i, 0});
+        }
+        while(k-- && !pq.empty()){
+            auto [sum,i,j]=pq.top();
+            pq.pop();
+            ans.push_back({nums1[i],nums2[j]});
+            if(j+1<m){
+                pq.push({nums1[i]+nums2[j+1],i,j+1});
+            }
+        }
+        return ans;
+    }
+};
+
+//This is thinkable
 //TLE - Max Heap
 class Solution {//Brute force and Optimised Brute Force
 public:
@@ -7,7 +31,8 @@ public:
             for(int b: nums2){
                 pq.emplace(a+b,make_pair(a,b));
                 if(pq.size()>k) pq.pop();
-                /*This exploits the sorted vectors info, and is an enhancement, This code runs but is not efficient, has the same worse case TC
+                //No TLE - enhancement
+                /*This exploits the sorted vectors info, and is an enhancement, This code runs but has the same worse case TC.
                 if(pq.size()<k) pq.emplace(a+b,make_pair(a,b));
                 else if(a+b<pq.top().first){
                     pq.pop();
@@ -22,6 +47,37 @@ public:
             auto [sum,p]=pq.top();
             pq.pop();
             ans.push_back({p.first,p.second});
+        }
+        return ans;
+    }
+};
+
+//This is very non intuitive
+//Min heap + set
+class Solution {
+public:
+    typedef pair<int,pair<int,int>> p;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {//TC=O(klogk), SC=O(k)
+        int n=nums1.size(),m=nums2.size();
+        vector<vector<int>> ans;
+        set<pair<int,int>> visited;
+        priority_queue<p,vector<p>,greater<p>> pqmh;
+        pqmh.push({nums1[0]+nums2[0],{0,0}});
+        visited.insert({0,0});
+        while(k-- && !pqmh.empty()){
+            auto top=pqmh.top();
+            pqmh.pop();
+            int i=top.second.first;
+            int j=top.second.second;
+            ans.push_back({nums1[i],nums2[j]});
+            if(i+1<n && visited.find({i+1,j})==visited.end()){
+                visited.insert({i+1,j});
+                pqmh.push({nums1[i+1]+nums2[j],{i+1,j}});
+            }
+            if(j+1<m && visited.find({i,j+1})==visited.end()){
+                visited.insert({i,j+1});
+                pqmh.push({nums1[i]+nums2[j+1],{i,j+1}});
+            }
         }
         return ans;
     }
