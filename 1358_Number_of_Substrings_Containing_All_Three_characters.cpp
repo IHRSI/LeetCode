d2b4f3fe2185ec0ba58f150b2c73ad48408da6cc
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-07-02 01:20:22 +0530

    Done(MED[POTD] - sliding window, hashing, string
    Added an alternative implementation using last position tracking for counting substrings containing all three characters.

--- 1358_Number_of_Substrings_Containing_All_Three_characters.cpp
+++ 1358_Number_of_Substrings_Containing_All_Three_characters.cpp
@@ -1,3 +1,4 @@
+//Sliding Window
 class Solution {
 public:
     int numberOfSubstrings(string s) {//TC=O(2n)~O(n), SC=O(1)
@@ -14,5 +15,28 @@ public:
             ++r;
         }
         return ans;
+    }
+};
+
+//Last Position Tracking
+class Solution {
+public:
+    int numberOfSubstrings(string s) {//TC=O(n), SC=O(1)
+        int len = s.length();
+        // Track last position of a, b, c
+        vector<int> lastPos = {-1, -1, -1};
+        int total = 0;
+
+        for (int pos = 0; pos < len; pos++) {
+            // Update last position of current character
+            lastPos[s[pos] - 'a'] = pos;
+
+            // Add count of valid substrings ending at current position
+            // If any character is missing, min will be -1
+            // Else min gives leftmost required character position
+            total += 1 + min({lastPos[0], lastPos[1], lastPos[2]});
+        }
+
+        return total;
     }
 };
