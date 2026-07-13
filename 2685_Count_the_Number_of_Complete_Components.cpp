0bc6a684fdfa1988efa91c4490b83826c2e3f703
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-07-13 22:05:12 +0530

    Create 2685_Count_the_Number_of_Complete_Components.cpp

---
+++ 2685_Count_the_Number_of_Complete_Components.cpp
@@ -0,0 +1,78 @@
+//Using the formula V*(V-1)=2E check is better tran checking the each component vertex degree which should equal V-1 as it saves space and comptation after traversal.
+//We use formula in the dfs approach and the other way in the bsf approach. Both ways are there to check the connected component.
+//DFS
+class Solution {
+public:
+    void dfs(int node,vector<vector<int>> &adj, vector<bool> &vis, pair<int,int> &p){//TC=O(V+2E)~O(V+E), SC=O(V)
+        if(vis[node]) return;
+        vis[node]=1;
+        ++p.first;
+        p.second+=adj[node].size();
+        for(int chi: adj[node]){
+            dfs(chi,adj,vis,p);
+        }
+        return;
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {//TC=O(E+V+2E)~O(V+E), SC=O(V+2E+V+V)~O(V+E)
+        vector<vector<int>> adj(n);
+        for(int i=0;i<edges.size();++i){
+            int a=edges[i][0],b=edges[i][1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        int ct=0;
+        vector<bool> vis(n);
+        for(int i=0;i<n;++i){
+            if(!vis[i]){
+                pair<int,int> p;
+                dfs(i,adj,vis,p);
+                if(p.first*(p.first-1)==p.second) ++ct;
+            }
+        }
+        return ct;
+    }
+};
+
+//BFS
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {//TC=O(E+V+2E+V)~O(V+E), SC=O(V+2E+V+V+V)~O(V+E)
+        vector<vector<int>> adj(n);
+        for(int i=0;i<edges.size();++i){
+            int a=edges[i][0],b=edges[i][1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        int ct=0;
+        vector<bool> vis(n);
+        for(int i=0;i<n;++i){
+            if(!vis[i]){
+                vector<int> comp;
+                queue<int> q;
+                q.push(i);
+                vis[i]=1;
+                while(!q.empty()){
+                    int curr=q.front();
+                    q.pop();
+                    comp.push_back(curr);
+                    for(int nei:adj[curr]){
+                        if(!vis[nei]){
+                            q.push(nei);
+                            vis[nei]=1;
+                        }
+                    }
+                }
+                bool iscomp=1;
+                int compsz=comp.size();
+                for(int node:comp){
+                    if(adj[node].size()!=compsz-1){
+                        iscomp=0;
+                        break;
+                    }
+                }
+                if(iscomp) ++ct;
+            }
+        }
+        return ct;
+    }
+};
