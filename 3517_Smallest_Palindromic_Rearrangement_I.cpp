ecb0717e830a97f8801e49a44ff71253f14681f6
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-07-28 11:06:58 +0530

    Done(MED)[POTD] - counting sort, string, sort

--- 3517_Smallest_Palindromic_Rearrangement_I.cpp
+++ 3517_Smallest_Palindromic_Rearrangement_I.cpp
@@ -1,4 +1,5 @@
 //Self Coded - Most Efficient
+//Eventually concept used is counting sort
 class Solution {
 public:
     string smallestPalindrome(string s) {//TC=O(n), SC=O(1)
@@ -7,8 +8,6 @@ public:
         for(int i=0;i<n/2;++i){
             ++arr[s[i]-'a'];
         }
-        char mid;
-        if(n&1) mid=s[n/2];
         int k=0;
         for(int i=0;i<26;++i){
             if(arr[i]){
@@ -20,10 +19,7 @@ public:
                 }
             }
         }
-        if(n&1){
-            s[k]=mid;
-            ++k;
-        }
+        if(n&1) ++k;
         for(int i=25;i>=0;--i){
             if(arr[i]){
                 int ct=arr[i];
@@ -33,6 +29,42 @@ public:
                     ++k;
                 }
             }
+        }
+        return s;
+    }
+};
+
+//Concised code - Used the mirror property of palindrome
+class Solution {
+public:
+    string smallestPalindrome(string s) {//TC=O(n), SC=O(1)
+        int n=s.size();
+        int arr[26]={0};
+        for(int i=0;i<n/2;++i){
+            ++arr[s[i]-'a'];
+        }
+        int l=0,r=n-1;
+        for(int i=0;i<26;++i){
+            int ct=arr[i];
+            while(ct>0){
+                char c='a'+i;
+                s[l++]=c;
+                s[r--]=c;
+                --ct;
+            }
+        }
+        return s;
+    }
+};
+
+//Sorting
+class Solution {//TC=O(n/2 + (n/2)log(n/2)), SC=O(logn)
+public:
+    string smallestPalindrome(string s) {
+        int n=s.size();
+        sort(s.begin(),s.begin()+n/2);
+        for(int i=0;i<n/2;++i){
+            s[n-1-i]=s[i];
         }
         return s;
     }
