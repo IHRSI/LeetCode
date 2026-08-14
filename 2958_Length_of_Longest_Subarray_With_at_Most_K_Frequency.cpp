f615b2e1ad231d07f608c81ae8ec61b76770c3c9
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-08-14 22:44:29 +0530

    Done(MED)[POTD] - sliding window, hash(um)

--- 2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency.cpp
+++ 2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency.cpp
@@ -20,3 +20,23 @@ public:
         return ans;
     }
 };
+
+//Simplified from above code - removed the if else
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {//TC=O(n), SC=O(n)
+        int n=nums.size();
+        unordered_map<int,int> um;
+        int ans=0,i=0,j=0;
+        while(j<n){
+            ++um[nums[j]];
+            while(um[nums[j]]>k){
+                --um[nums[i]];
+                ++i;
+            }
+            ans=max(ans,j-i+1);
+            ++j;
+        }
+        return ans;
+    }
+};
