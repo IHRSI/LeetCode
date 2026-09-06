16826d3e1e40ea84a635405e53deb10bd234f65e
Hiten Raj Singh <147341843+IHRSI@users.noreply.github.com>
2026-09-06 17:25:52 +0530

    Done(EMED)[POTD] - prefix sum

---
+++ 3904_Smallest_Stable_Index_II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {//TC=O(2n)~O(n), SC=O(n)
+        int n=nums.size();
+        vector<int> minsuff(n);
+        minsuff[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;--i){
+            minsuff[i]=min(minsuff[i+1],nums[i]);
+        }
+        int maxpre=INT_MIN;
+        for(int i=0;i<n;++i){
+            maxpre=max(maxpre,nums[i]);
+            if((maxpre-minsuff[i])<=k) return i;
+        }
+        return -1;
+    }
+};
